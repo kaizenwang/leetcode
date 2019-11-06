0c3e2a6c5000434ebe653353f46fe3ec18e412a7
Kai <wangkai.kaizen@gmail.com>
2019-11-06 11:48:59 +0800

    Initial commit

---
+++ algorithms/java/src/removeDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,9 @@
+package removeDuplicatesFromSortedArray;
+
+/**
+ * @author kaizen
+ * @date 2019/11/6
+ */
+public class RemoveDuplicatesFromSortedArray {
+
+}
---
+++ algorithms/java/src/removeDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArrayTest.java
@@ -0,0 +1,15 @@
+package removeDuplicatesFromSortedArray;
+
+import org.junit.Test;
+
+/**
+ * @author kaizen
+ * @date 2019/11/6
+ */
+public class RemoveDuplicatesFromSortedArrayTest {
+
+    @Test
+    public void testSolution(){
+
+    }
+}
