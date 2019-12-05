cd5accb035c1b5fae44964b4d15c88119e7913b8
Kai <wangkai.kaizen@gmail.com>
2019-12-05 21:21:10 +0800

    ...

--- algorithms/java/src/removeDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArrayTest.java
+++ algorithms/java/src/removeDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArrayTest.java
@@ -10,11 +10,11 @@ import org.junit.Test;
  */
 public class RemoveDuplicatesFromSortedArrayTest {
 
-  private RemoveDuplicatesFromSortedArray fun;
+  private RemoveDuplicatesFromSortedArray3 fun;
 
   @Before
   public void init() {
-    fun = new RemoveDuplicatesFromSortedArray();
+    fun = new RemoveDuplicatesFromSortedArray3();
   }
 
   @Test
@@ -32,6 +32,16 @@ public class RemoveDuplicatesFromSortedArrayTest {
     int[] nums = new int[] {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
     int len = fun.removeDuplicates(nums);
     int[] expected = new int[] {0, 1, 2, 3, 4};
+    for (int i = 0; i < len; i++) {
+      Assert.assertEquals(expected[i], nums[i]);
+    }
+  }
+
+  @Test
+  public void test2RemoveDuplicates() {
+    int[] nums = new int[] {0, 1, 1, 2};
+    int len = fun.removeDuplicates(nums);
+    int[] expected = new int[] {0, 1, 2};
     for (int i = 0; i < len; i++) {
       Assert.assertEquals(expected[i], nums[i]);
     }
