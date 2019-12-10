93d717197903660e141d6c2995503ffd8a5fa45d
Kai <wangkai.kaizen@gmail.com>
2019-12-10 20:05:59 +0800

    add three sum test

--- algorithms/java/src/threeSum/ThreeSumTest.java
+++ algorithms/java/src/threeSum/ThreeSumTest.java
@@ -16,34 +16,42 @@ import static org.junit.Assert.*;
  */
 public class ThreeSumTest {
 
-  private ThreeSum2 threeSum;
+    private ThreeSum2 threeSum;
 
-  @Before
-  public void init() {
-    threeSum = new ThreeSum2();
-  }
+    @Before
+    public void init() {
+        threeSum = new ThreeSum2();
+    }
 
-  @Test
-  public void threeSum() {
-    int[] nums = new int[] {-1, 0, 1, 2, -1, -4};
-    List<List<Integer>> expected = new ArrayList<>();
-    expected.add(Arrays.asList(-1, -1, 2));
-    expected.add(Arrays.asList(-1, 0, 1));
-    assertThat(threeSum.threeSum(nums), is(expected));
-  }
+    @Test
+    public void threeSum() {
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(-1, -1, 2));
+        expected.add(Arrays.asList(-1, 0, 1));
+        assertThat(threeSum.threeSum(nums), is(expected));
+    }
 
-  @Test
-  public void testThreeSum() {
-    int[] nums = new int[] {0, 0, 0};
-    List<List<Integer>> expected = new ArrayList<>();
-    expected.add(Arrays.asList(0, 0, 0));
-    assertThat(threeSum.threeSum(nums), is(expected));
-  }
+    @Test
+    public void testThreeSum() {
+        int[] nums = new int[]{0, 0, 0};
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(0, 0, 0));
+        assertThat(threeSum.threeSum(nums), is(expected));
+    }
 
-  @Test
-  public void test1ThreeSum() {
-    int[] nums = new int[] {1, 2, -2, -1};
-    List<List<Integer>> expected = new ArrayList<>();
-    assertThat(threeSum.threeSum(nums), is(expected));
-  }
+    @Test
+    public void test1ThreeSum() {
+        int[] nums = new int[]{1, 2, -2, -1};
+        List<List<Integer>> expected = new ArrayList<>();
+        assertThat(threeSum.threeSum(nums), is(expected));
+    }
+
+    @Test
+    public void test2ThreeSum() {
+        int[] nums = new int[]{-1, -1, 0, 1};
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(-1, 0, 1));
+        assertThat(threeSum.threeSum(nums), is(expected));
+    }
 }
